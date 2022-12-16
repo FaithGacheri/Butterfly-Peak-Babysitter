93b8c08812a19947cba698d494c705b9090e4025
kimuit117 <kemoikiuita@gmail.com>
2022-12-16 19:04:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './Components/NavBar';
-import Home from './Components/Home';
-import About from './Components/About';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import Contact from './Components/Contact';
+// import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import NavBar from './Components/NavBar';
+// import Home from './Components/Home';
+// import About from './Components/About';
+// import Login from './Components/Login';
+// import Signup from './Components/Signup';
+// import Contact from './Components/Contact';
+import Cards from "./Components/Cards";
 
 function App() {
   return (
   <>
-  <NavBar/>
+  {/* <NavBar/>
   <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
     <Route path="/contact" element={<Contact/>} />
     <Route path="/login" element={<Login/>} />
     <Route path="/sign_up" element={<Signup/>} />
-  </Routes>
+  </Routes> */}
+  <Cards/>
   </>
   );
 }
