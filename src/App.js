2d3296abcab4399afb016c8b9d4273860b12fe4d
iSaacSigei <kiplangatisack.is@gmail.com>
2022-12-16 16:58:25 +0300

    updated the routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   <>
   <NavBar/>
   <Routes>
-    <Route path="/available_nannies" element={<Home/>} />
+    <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
     <Route path="/contact" element={<Contact/>} />
     <Route path="/login" element={<Login/>} />
