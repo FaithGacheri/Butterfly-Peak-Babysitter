36789fc699cd34a09c0d37b7a7f6af580c8e32ee
“[firstname <faith.gacheri@student.moringaschool.com>
2022-12-18 15:17:01 +0300

    add h1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='g-about'>
+        <div>
+            <h1>Get your baby well taken care of Here</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quidem, asperiores fugiat maiores, ducimus vero porro beatae, similique facere ratione voluptatem. Ex voluptatem distinctio provident officia inventore qui, excepturi mollitia?
+            
+        </div>
+    </div>
   )
 }
 
