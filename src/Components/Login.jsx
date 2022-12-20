94b4bf0fb8a5d39d3388e49726c9f7d5b5875245
iSaacSigei <kiplangatisack.is@gmail.com>
2022-12-20 15:41:22 +0300

    minor changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,8 +5,8 @@ export default function Login() {
     <>
       <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8 overflow-hidden">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
-            Login in to your account
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-indigo-600">
+            Login to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
