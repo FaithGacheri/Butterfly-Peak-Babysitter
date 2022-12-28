22a9f7da9d846d147ee9540aaa5b8f29e90ddb24
iSaacSigei <kiplangatisack.is@gmail.com>
2022-12-28 23:31:50 +0300

    media queries for testimonials

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -13,14 +13,14 @@ export default function Testimonials() {
       </div>
 
       <div className="bg-white m-auto w-4/5 lg:py-24 grid lg:grid-cols-3 lg:grid-rows-auto gap-x-3.5 lg:gap-y-3.5 ">
-        <div className="bg-gradient-to-r from-teal-500 to-cyan-600 lg:relative lg:z-10 lg:pb-0 m-auto sm:flex-col mt-10 md:pb-8">
+        <div className="bg-gradient-to-r from-teal-500 to-cyan-600 lg:relative lg:z-10 lg:pb-0 m-auto sm:flex-col max-md:flex-col mt-10 md:pb-8">
           <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
             <div className="relative lg:-my-8">
               <div
                 aria-hidden="true"
-                className="absolute inset-x-0 top-0 h-1/2 bg-white sm:hidden"
+                className="absolute inset-x-0 top-0  bg-white sm:hidden"
               />
-              <div className="mx-auto max-w-md px-6 sm:max-w-3xl sm:px-6 lg:h-1/2 lg:w-full lg:p-0">
+              <div className="mx-auto px-6 sm:max-w-3xl sm:px-6 lg:h-1/2 lg:w-full lg:p-0">
                 <div className="aspect-w-10 aspect-h-6 overflow-hidden rounded-xl shadow-xl sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
                   <img
                     className="object-cover lg:h-full lg:w-full"
