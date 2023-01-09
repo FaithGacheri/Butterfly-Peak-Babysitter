b873c8666f0026162a3ed0a636492e983efc2229
“[firstname <faith.gacheri@student.moringaschool.com>
2023-01-09 20:32:32 +0300

    change blog classnames

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -9,10 +9,10 @@ function Blog() {
       <p>
         This blog is a collection of blog posts. It is used by blog posts only
       </p> */}
-      <div className="section">
-        <div className="card">
-          <div className="image-section img-one"></div>
-          <div className="content">
+      <div className="g-section">
+        <div className="g-card">
+          <div className="g-image-section img-one"></div>
+          <div className="g-content">
             <h2>Title One</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum debitis, ad ratione error at animi optio odio dolores dolor earum atque, facilis voluptatem quas eveniet maiores quaerat enim voluptatibus sapiente?</p>
             <Link to="/blog/blog1"><button id="g-btn">Read More</button></Link>
@@ -20,9 +20,9 @@ function Blog() {
           </div>
         </div>
 
-        <div className="card">
-          <div className="image-section img-two"></div>
-          <div className="content">
+        <div className="g-card">
+          <div className="g-image-section img-two"></div>
+          <div className="g-content">
             <h2>Title Two</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum debitis, ad ratione error at animi optio odio dolores dolor earum atque, facilis voluptatem quas eveniet maiores quaerat enim voluptatibus sapiente?</p>
             <button id="g-btn">Read More</button>
@@ -30,9 +30,9 @@ function Blog() {
           </div>
         </div>
 
-        <div className="card">
-          <div className="image-section img-three"></div>
-          <div className="content">
+        <div className="g-card">
+          <div className="g-image-section img-three"></div>
+          <div className="g-content">
             <h2>Title Three</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum debitis, ad ratione error at animi optio odio dolores dolor earum atque, facilis voluptatem quas eveniet maiores quaerat enim voluptatibus sapiente?</p>
             <button id="g-btn">Read More</button>
