13a006db3897b0360a1215a3e2edd8e47ebc56e5
iSaacSigei <kiplangatisack.is@gmail.com>
2023-01-09 14:09:06 +0300

    modifidied cards

--- src/Components/AvailableNannys.js
+++ src/Components/AvailableNannys.js
@@ -3,135 +3,105 @@ import { Link } from "react-router-dom";
 import "../Styles/Cards.css";
 import Search from "./Search";
 
+export default function AvailableNannys() {
+  const [nannyData, setNannyData] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [card, setCard]=useState(0);
+  console.log(card);
 
+  useEffect(() => {
+    fetch(`http://localhost:3000/nannyData`)
+      .then((res) => res.json())
+      .then((nannyData) => {
+        // console.log(nannyData)
+        setNannyData(nannyData);
+        setSearchResult(nannyData);
+      });
+  }, []);
 
-export default function AvailableNannys () {
-    const [nannyData, setNannyData] = useState([]);
-    const [searchResult, setSearchResult] = useState([])
-   
-    useEffect(() => {
-        fetch(`http://localhost:3000/nannyData`)
-            .then((res) => res.json())
-            .then((nannyData) => {
-                // console.log(nannyData)
-                setNannyData(nannyData)
-                setSearchResult(nannyData)
-            })
-    }, [])
+  //event listener watching change on search component
+  const change = (e) => {
+    setNannyData(
+      searchResult.filter((nannyData) =>
+        nannyData.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  };
+  function favouritenanny(data) {
+    fetch(`http://localhost:3000/favouriteNannys`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        name: data.name,
+        nannyLocation: data.nannyLocation,
+        rating: data.rating,
+        nannyPrice: data.nannyPrice,
+        nanny_url: data.nanny_url,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err.message));
+  }
 
-    //event listener watching change on search component
-    const change = (e) => {
-        setNannyData(searchResult.filter(nannyData =>
-            nannyData.name.toLowerCase()
-                .includes(e.target.value.toLowerCase())))
-
-    }
-    function favouritenanny(data) {
-        fetch(`http://localhost:3000/favouriteNannys`, {
-            method: 'POST',
-            headers: {"content-type":"application/json"},
-            body: JSON.stringify({
-                name:data.name,
-                nannyLocation:data.nannyLocation,
-                rating:data.rating,
-                nannyPrice:data.nannyPrice,
-                nanny_url:data.nanny_url
-            })
-
-        } )
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch(err => console.error(err.message))
-
+  const nanny= nannyData.map((data, key) =>
     
-    }
-    return (
-        <>
- 
-            <Search change={change} />
-       
-            <div className="stock-container">
-                {nannyData.map((data, key) => {
-                    return (
-                        <div key={key}>
-                            <Nanny
-                                key={key}
-                                data={data}
-                                name={data.name}
-                                nannyLocation={data.nannyLocation}
-                                rating={data.rating}
-                                nannyPrice={data.nannyPrice}
-                                nanny_url={data.nanny_url}
-                                favouritenanny={favouritenanny}
-                            />
-                        </div>
-                    );
-                })}
+      <div className="ui four column grid all">
+      <div className="ui column">
+        <div className="ui card">
+          <div className="image">
+            <img alt="Image not found" src={data.nanny_url} />
+          </div>
+
+          <div className="content">
+            <div className="header">{data.name}</div>
+            <div className="meta text-wrap">
+              <small>{data.nannyLocation}</small>
             </div>
-        </>
-    );
-
-};
-// const favouriteNanny
-const Nanny = ({ name, data, nannyLocation, rating, nannyPrice, nanny_url, favouritenanny}) => {
-  
-   console.log(data);
-    if (!name) return <div />;
-    return (
-  
- <div className="ui four column grid all">
-        <div className="ui column" >
-            <div className="ui card">
-                <div className="image">
-                    <img alt="Image not found" src={nanny_url} />
-                </div>
-
-            <div className="content">
-             <div className="header">
-               {name}
-             </div>
-                <div className="meta text-wrap">
-                   <small>{nannyLocation}</small>
-                 </div>
-                 <div className="meta text-wrap">
-                   <small>{rating}</small>
-                 </div>
-                 <div className="meta text-wrap">
-                   <small>{nannyPrice}</small>
-                 </div>
+            <div className="meta text-wrap">
+              <small>{data.rating}</small>
             </div>
+            <div className="meta text-wrap">
+              <small>{data.nannyPrice}</small>
+            </div>
+          </div>
 
+          <div className="extra content">
+            <span>
+              <div className="ui center aligned segment basic">
+                <button className="ui mini  button">Book</button>
 
-         <div className="extra content">
-              <span>
-                <div className="ui center aligned segment basic">
-                    <button
-                    className="ui mini  button"
-                    >
-                    Book
-                    </button>
-
-                    <button
-                       className="fav-button"
-                       onClick={()=>favouritenanny(data)}
-                    >   
-                       Favourite
-                     </button>
-                 <div className="view more">
-                  <button>
+                <button
+                  className="fav-button"
+                  onClick={() => favouritenanny(data)}
+                >
+                  Favourite
+                </button>
+                <div className="#">
+                  <button onClick={()=>setCard(data.id)}>
                     <small>
-                        <Link to="/card-item">
+                      <Link to="/cards/card-item">
                         <small>view more...</small>
-                        </Link></small></button>
-                 </div>
+                      </Link>
+                    </small>
+                  </button>
                 </div>
-              </span>
-         </div>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+  return (
+    <>
+      <Search change={change} />
+
+      <div className="stock-container">
+        {nanny}
+      </div>
+    </>
+  );
+}
 
 
-        </div>
-        </div>
-     </div> 
-     
-    );
-    };
