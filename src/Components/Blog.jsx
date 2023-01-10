9621bf6d8bb0119de9ba90179c3ef6eca2880d55
“[firstname <faith.gacheri@student.moringaschool.com>
2023-01-10 11:59:07 +0300

    blog3 updated

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -35,7 +35,7 @@ function Blog() {
           <div className="g-content">
             <h2>Title Three</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum debitis, ad ratione error at animi optio odio dolores dolor earum atque, facilis voluptatem quas eveniet maiores quaerat enim voluptatibus sapiente?</p>
-            <button id="g-btn">Read More</button>
+            <Link to="/blog/blog3"><button id="g-btn">Read More</button></Link>
 
           </div>
         </div>
