f429bfcecd0fdb66ff49814dabcb550dfcfc5086
iSaacSigei <kiplangatisack.is@gmail.com>
2023-01-10 10:26:02 +0300

    routing for checkout status page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './Components/NavBar';
-import Home from './Components/Home';
-import About from './Components/About';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import Blog from './Components/Blog'; 
-import Contact from './Components/Contact';
-import ForgotPasswordForm from './Components/ForgotPasswordForm'
-import Blog1 from './Components/Blog1';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Blog from "./Components/Blog";
+import Contact from "./Components/Contact";
+import ForgotPasswordForm from "./Components/ForgotPasswordForm";
+import Blog1 from "./Components/Blog1";
 import AvailableNannys from "./Components/AvailableNannys";
 import Favourite from "./Components/Favourite";
 import Caregiver from "./Components/CaregiverForm";
-import CardItem from './Components/CardItem'
-import Checkout from './Components/Checkout'
-
+import CardItem from "./Components/CardItem";
+import Checkout from "./Components/Checkout";
+import CheckoutstatusPage from "./Components/CheckoutStatusPage";
 
 function App() {
   return (
-  <div >
-  <NavBar/>
-  <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/about" element={<About/>} />
-    <Route path="/blog" element={<Blog/>} />
-    <Route path="blog">
-      <Route path="blog1" element={<Blog1/>} />
-    </Route>
-    <Route path="/contact" element={<Contact/>} />
-    <Route path="/login" element={<Login/>} />
-    <Route path='/login/forgot_password' element={<ForgotPasswordForm/>}/>
-    <Route path="/sign_up" element={<Signup/>} />
-    <Route exact path="/">
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="blog">
+          <Route path="blog1" element={<Blog1 />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/forgot_password" element={<ForgotPasswordForm />} />
+        <Route path="/sign_up" element={<Signup />} />
+        <Route exact path="/">
           <Route exact path="/checkout" element={<Checkout />} />
           <Route exact path="/cards" element={<AvailableNannys />} />
           <Route exact path="/favourite" element={<Favourite />} />
@@ -43,11 +42,12 @@ function App() {
         <Route path="cards">
           <Route path="card-item" element={<CardItem />} />
         </Route>
-  </Routes>
-  </div>
-
-
-  )
+        <Route path="checkout">
+          <Route path="checkout_status" element={<CheckoutstatusPage/>} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
