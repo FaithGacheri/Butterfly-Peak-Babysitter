eb899f7fadf7bb07fa4eafc3e4bc4166b1886210
iSaacSigei <kiplangatisack.is@gmail.com>
2023-01-17 13:21:23 +0300

    routes for both users

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
-import Service from './Service';
-import Testimonials from './Testimonials';
-import Footer from './Footer';
-import Contact from './Contact'
-function Home() {
+import Service from "./Service";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import Contact from "./Contact";
+function Home({ user, caregiver }) {
   return (
     <div>
-         <main>
-          <div className=" h-screen bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
-            <div className="mx-auto w-4/5 lg:px-8">
-              <div className="lg:grid lg:grid-cols-2 lg:gap-10">
-                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
-                  <div className="lg:py-24">
-                    <Link
+      <main>
+        <div className=" h-screen bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
+          <div className="mx-auto w-4/5 lg:px-8">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-10">
+              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
+                <div className="lg:py-24">
+                  {user?
+                    (<Link
                       to="/cards"
                       className="inline-flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
                     >
@@ -28,85 +29,99 @@ function Home() {
                         className="ml-2 h-5 w-5 text-gray-500"
                         aria-hidden="true"
                       />
+                    </Link>):(
+                      <Link
+                      to="/caregiver"
+                      className="inline-flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
+                    >
+                      <span className="rounded-full bg-gradient-to-r from-teal-500 to-cyan-600 px-5 py-0.5 text-sm font-semibold leading-10 text-white">
+                        Bookings?
+                      </span>
+                      <span className="ml-4 text-sm">
+                        Check recent bookings
+                      </span>
+                      <ChevronRightIcon
+                        className="ml-2 h-5 w-5 text-gray-500"
+                        aria-hidden="true"
+                      />
                     </Link>
-                    <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                      <span className="block">
-                        Want your baby well taken care of?
-                      </span>
-                      <span className="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5">
-                        we got you
-                      </span>
-                    </h1>
-                    <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-                      An exemplary baby-sitting service company, we recruit the
-                      best nannies, and at your request, they are avail to your home address as you as possible
-                    </p>
-                    <div className="sm:mt-12 ml-0">
-                      <form
-                        action="POST"
-                        className="sm:mx-auto lg:mx-0"
-                      >
-                        <div className="flex items-center m-0">
-                          <div className="min-w-0 flex-1">
-                            <label htmlFor="email" className="sr-only">
-                              Email address
-                            </label>
-                            <input
-                              id="email"
-                              type="email"
-                              placeholder="Enter your email"
-                              className="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
-                            />
-                          </div>
-                          <div className="sm:mt-0 sm:ml-3">
-                            <Link to="/sign_up">
-                            <button
-                              className="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
-                            >
+
+                    )
+                  }
+                  <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                    <span className="block">
+                      Want your baby well taken care of?
+                    </span>
+                    <span className="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5">
+                      we got you
+                    </span>
+                  </h1>
+                  <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+                    An exemplary baby-sitting service company, we recruit the
+                    best nannies, and at your request, they are avail to your
+                    home address as you as possible
+                  </p>
+                  <div className="sm:mt-12 ml-0">
+                    <form action="POST" className="sm:mx-auto lg:mx-0">
+                      <div className="flex items-center m-0">
+                        <div className="min-w-0 flex-1">
+                          <label htmlFor="email" className="sr-only">
+                            Email address
+                          </label>
+                          <input
+                            id="email"
+                            type="email"
+                            placeholder="Enter your email"
+                            className="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+                          />
+                        </div>
+                        <div className="sm:mt-0 sm:ml-3">
+                          <Link to="/sign_up">
+                            <button className="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900">
                               Get Started
                             </button>
-                            </Link>
-                          </div>
-                        </div>
-                        <p className="mt-3 text-xl text-gray-300 sm:mt-4">
-                          Register and get 2Day care for free, no credit card
-                          necessary. By providing your email, you agree to our{" "}
-                          <Link to="/services" className="font-medium text-white">
-                            terms of service
                           </Link>
-                          .
-                        </p>
-                        </form>
-                     
-                    
-                    </div>
-                    <div>
-                      <Link to="/cards">
-                      <button className="flex justify-center w-1/2 rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900 mt-12">Visit Babysitters page</button>
-                      </Link>
-                    </div>
+                        </div>
+                      </div>
+                      <p className="mt-3 text-xl text-gray-300 sm:mt-4">
+                        Register and get 2Day care for free, no credit card
+                        necessary. By providing your email, you agree to our{" "}
+                        <Link to="/services" className="font-medium text-white">
+                          terms of service
+                        </Link>
+                        .
+                      </p>
+                    </form>
+                  </div>
+                  <div>
+                    <Link to="/cards">
+                      <button className="flex justify-center w-1/2 rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900 mt-12">
+                        Visit Babysitters page
+                      </button>
+                    </Link>
                   </div>
                 </div>
-                <div className="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
-                  <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                    <img
-                      className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-full lg:max-w-none"
-                      src="https://images.squarespace-cdn.com/content/v1/5a2f40a41f318d38ccf0c819/1593066487695-U1K0I1UIO6IL230ECKKO/Website-Redesign_KidsCarePackV2.0-10.png?format=2500w"
-                      alt=""
-                    />
-                  </div>
+              </div>
+              <div className="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                  <img
+                    className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-full lg:max-w-none"
+                    src="https://images.squarespace-cdn.com/content/v1/5a2f40a41f318d38ccf0c819/1593066487695-U1K0I1UIO6IL230ECKKO/Website-Redesign_KidsCarePackV2.0-10.png?format=2500w"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
           </div>
-        </main>
-     
-        <Service/>
-        <Testimonials/>
-        <Contact/>
-        <Footer/>
+        </div>
+      </main>
+
+      <Service />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
