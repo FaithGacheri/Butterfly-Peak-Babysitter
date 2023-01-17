2afc69b3d7c0a38b1689d4e05db7f526609f51a8
iSaacSigei <kiplangatisack.is@gmail.com>
2023-01-17 10:13:10 +0300

    minor styling changes

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import { useParams } from "react-router-dom";
 
-export default function CardItem({user, data}) {
+export default function CardItem({ user, data }) {
   const [nanny, setCaregiver] = useState({});
   const { id } = useParams();
   const [reviews, setReviews] = useState([]);
-  const [images, setImages]=useState({})
-  const [status, setStatus]=useState(null)
+  const [images, setImages] = useState({});
+  const [status, setStatus] = useState(null);
+  const [location, setLocation] = useState({});
+
+  console.log(id)
   useEffect(() => {
     fetch(`/caregivers/${id}`)
       .then((r) => r.json())
@@ -15,17 +18,26 @@ export default function CardItem({user, data}) {
         console.log(data);
         setCaregiver(data);
         setReviews(data.reviews);
-        setImages(data.image)
-        setStatus(data.is_booked)
+        setImages(data.image);
+        setStatus(data.is_booked);
+        setLocation(data.location);
       });
   }, []);
 
-
+  console.log(reviews)
   const r = reviews.map((rating) => rating.ratings);
 
   return (
     <>
-      <Card nanny={nanny} r={r} images={images} status={status} user={user} data={data}/>
+      <Card
+        nanny={nanny}
+        r={r}
+        images={images}
+        status={status}
+        user={user}
+        data={data}
+        location={location}
+      />
     </>
   );
 }
