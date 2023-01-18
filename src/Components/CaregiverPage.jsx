3ab695ac3fafcd217f630d53795b951daf22d83f
iSaacSigei <kiplangatisack.is@gmail.com>
2023-01-18 23:34:38 +0300

    updated the caregiver status page

--- src/Components/CaregiverPage.jsx
+++ src/Components/CaregiverPage.jsx
@@ -1,56 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Styles/CaregiverPage.css";
-function CaregiverPage() {
-  const [click, setClick] = useState(false);
+import CaregiverPersonalBoooking from "./CaregiverPersonalBooking";
+function CaregiverPage({ user }) {
+  const [bookings, setBookings] = useState([]);
 
+  useEffect(() => {
+    fetch(`/caregivers/${user.id}/bookings`)
+      .then((r) => r.json())
+      .then((data) => {
+        const trueBookings = data.filter(booking => booking.status === true);
+        setBookings(trueBookings);
+      });
+  }, []);
 
+console.log(bookings)
+// console.log(trueBookings);
   return (
     <>
-      <div className="c-page">
-        <div className="g22-card">
-          <div className="g22-image-section">
-            <img
-              className="h-48 w-48 rounded-full"
-              src="https://pbs.twimg.com/media/FhC3LvHXkAEMEUZ.png"
-              alt="Profile"
-            />
-          </div>
-
-          <div className="g22-content">
-            <div className="flex justify-between items-center ">
-              <h3 id="c-name">Name</h3>
-            </div>
-            {/* <Link to="/blog/blog1"><button id="g-btn">Read More</button></Link> */}
-          </div>
-        </div>
-        <div className="c-det">
-          <h2 className="md:text-3xl  pb-4">
-            Hurray!You have a pending appointment
-          </h2>
-          <div className="flex justify-between items-center mt-8 border-b">
-            <h3>Name</h3>
-          </div>
-
-          <div className="flex justify-between items-center mt-8 border-b">
-            <h3>Address </h3>
-          </div>
-          <div className="flex justify-between items-center mt-8 border-b">
-            <h3>Phone Number </h3>
-          </div>
-        </div>
-      </div>
-      <div className="c-btn">
-        <button onClick={() => setClick(true)} id="acc">
-          Accept
-        </button>
-        <button id="dec">Decline</button>
-      </div>
-      {click ? (
-        <div className="c-btn">
-          <button id="acc">Call</button>
-          <button id="mes">Message</button>
-        </div>
-      ) : null}
+      {bookings.map((booking)=>(<CaregiverPersonalBoooking key={booking.id} parent={booking} />))}
     </>
   );
 }
