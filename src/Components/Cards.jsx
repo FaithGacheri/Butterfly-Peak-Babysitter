b050cb92478096e8de530a466bda7b6b7578089c
kimuit117 <kemoikiuita@gmail.com>
2023-01-20 06:44:08 +0300

    removes the dropDown Bar

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import AvailableNannies from "./AvailableNannys";
 import Search from "./Search";
-import DropDownBar from "./DropDownBar";
 
 export default function Cards() {
   const [data, setData] = useState([]);
@@ -35,8 +34,7 @@ export default function Cards() {
   return (
     <>
       <Search change={change} />
-      <DropDownBar />
-      <h1 className="text-center text-gray-800 text-3xl">
+         <h1 className="text-center text-gray-800 text-3xl">
         Our Available Caregivers
       </h1>
       <div className="relative mx-auto mt-4 md:w-4/5 lg:grid lg:grid-cols-4 md:grid md:grid-cols-3 grid-rows-auto gap-x-10 row-gap-4.5 md:px-0 lg:px-0 md:gap-y-10">
